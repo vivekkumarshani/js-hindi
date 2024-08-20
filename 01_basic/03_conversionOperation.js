b780cf6ed336763924e1c91fbc3b0fde9204c3f8
Vivek Kumar <vivekkumarsahani169@gmail.com>
2024-08-20 18:55:19 +0530

    Second commit

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,33 @@
+"use Strict"; // treat all js code as newer version
+
+// alert(3+3)  wea are using nodejs, not browser
+
+
+console.log(3+3) // code readability should be high
+
+console.log("vivek")
+
+console.log("tested")
+let name="vivek"  //String
+let age = 25  // number
+
+let isLoggedIn = false // boolean
+let alsoHuge = BigInt(90025642362425345624455) // bigInt
+let userLogin = true/ false //boolean
+let userName; //undefined
+
+let userPassword = null //null
+
+
+// number => 2 to power 53
+// bigInt 
+// string
+// boolean => true/false
+// null => satndalone value
+// undefined
+// symbol
+
+// object
+
+
+// console.log(typeof "vivek")
