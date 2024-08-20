67f7dcf70e313e8cbe53b7ce0f9beb49619a613d
vivek kumar <110415752+vivekkumarshani@users.noreply.github.com>
2024-08-20 12:10:27 +0000

    operations evaluation

--- 01_basic/03_conversionOperation2.js
+++ 01_basic/03_conversionOperation2.js
@@ -3,9 +3,28 @@
 
 let score = "33abc"
 
-console.log(typeof score)
-console.log(typeof (score))
+// console.log(typeof score)
+// console.log(typeof (score))
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let booleanNumber = 1
+let convertToBoolen = Boolean(booleanNumber)
+// console.log(convertToBoolen)
+// console.log(typeof convertToBoolen)
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+// console.log(stringNumber)
+// console.log(typeof stringNumber)
+
+
+
+let someNew = "33abc"
+let stringNew = String(someNew)
+console.log(stringNew)
+console.log(typeof stringNew)
