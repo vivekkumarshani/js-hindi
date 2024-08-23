99b745bf559a56afd6e8a18d7206fdd21d59f5af
Vivek Kumar <vivekkumarsahani169@gmail.com>
2024-08-23 12:56:26 +0530

    IIFE basic knownledge

--- 03_basic/04_iife.js
+++ 03_basic/04_iife.js
@@ -1,6 +1,17 @@
 // Immediately Invoked function Expressions (IIFE)
 
-
+// Named IIFE
 (function chai(){
     console.log(`DB Connected`)
-})()
+})();
+
+// agr interview me puchhe ki DO IIFE kaise likhe to kaise likhenge, code execute hone me bad ";" lagna na bhule
+
+// normal IIFE 
+
+( (name)=>{
+    console.log(`DB CONNECT TWO ${name}`)
+})("vivek")
+
+
+// jaise hm function ke bad name defiend krte the variable me waise hi hmlog bina function define kiye hua v hm variable ko efine kr kte hain
